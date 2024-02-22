48967fe14bb58c6b3c8b9e89bf72e9a6ecd68637
wangjl <wangjl@7moor.com>
2024-02-22 23:49:30 +0800

    feat: 搭建框架

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author wangjl
+ * @date 2024/2/22
+ */
+@SpringBootApplication
+public class App
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+}
